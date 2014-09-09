14a4b900d4bf2c5482aafbc8af1b0abb2bfe7256
patito <benatto@gmail.com>
2014-09-09 20:38:02 +0100

    Entry point

--- app/elf/ehdr.py
+++ app/elf/ehdr.py
@@ -4,7 +4,10 @@ from ..binary_data import BinaryData, BinaryArch
 
 import binascii
 
-class EhdrFields(object):
+class EhdrFields64(object):
+    pass
+
+class EhdrFields32(object):
 
     TYPE = 15
     TYPE_SIZE = 17
@@ -14,6 +17,10 @@ class EhdrFields(object):
 
     VERSION = 19
     VERSION_SIZE = 21
+
+    ENTRY = 23
+    ENTRY_SIZE = 27
+
 
 class EhdrException(Exception):
     pass
@@ -29,17 +36,23 @@ class Ehdr32(object):
     def __init__(self, binary):
         self.binary = binary
 
-    def get_field(self, start, end):
-        return binascii.hexlify(str(self.binary[start:end]))
+    def get_field(self, start, end, direction=None):
+        if direction != None:
+            return binascii.hexlify(str(self.binary[start:end:direction]))
+        else:
+            return binascii.hexlify(str(self.binary[start:end]))
 
     def e_type(self):
-        return self.get_field(EhdrFields.TYPE, EhdrFields.TYPE_SIZE)
+        return self.get_field(EhdrFields32.TYPE, EhdrFields32.TYPE_SIZE)
 
     def e_machine(self):
-        return self.get_field(EhdrFields.MACHINE, EhdrFields.MACHINE_SIZE)
+        return self.get_field(EhdrFields32.MACHINE, EhdrFields32.MACHINE_SIZE)
 
     def e_version(self):
-        return self.get_field(EhdrFields.VERSION, EhdrFields.VERSION_SIZE)
+        return self.get_field(EhdrFields32.VERSION, EhdrFields32.VERSION_SIZE)
+
+    def e_entry(self):
+        return self.get_field(EhdrFields32.ENTRY_SIZE, EhdrFields32.ENTRY, -1)
 
 class Ehdr(object):
     """
@@ -54,6 +67,7 @@ class Ehdr(object):
                 print self.ehdr.e_type()
                 print self.ehdr.e_machine()
                 print self.ehdr.e_version()
+                print self.ehdr.e_entry()
             elif self.binary.arch == BinaryArch.ELFCLASS64:
                 self.ehdr = Ehdr64()
             else:
